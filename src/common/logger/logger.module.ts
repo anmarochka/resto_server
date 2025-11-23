5608ee816d9e4d8ead32d4d4f4a411547a1cee22
Anna Marochkina <annamarochkina2406@gmail.com>
2025-11-23 06:44:22 +0300

    add new

---
+++ src/common/logger/logger.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common"
+import { LoggerModule } from "nestjs-pino"
+
+@Module({
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== "production"
+            ? {
+                target: "pino-pretty",
+                options: {
+                  colorize: true,
+                  translateTime: "SYS:standard",
+                },
+              }
+            : undefined,
+      },
+    }),
+  ],
+})
+export class AppLoggerModule {}
