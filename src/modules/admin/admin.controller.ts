6165b2e02f437b9b5faba76f599462405545e76b
Anna Marochkina <annamarochkina2406@gmail.com>
2025-11-24 09:42:08 +0300

    add new

---
+++ src/modules/admin/admin.controller.ts
@@ -0,0 +1,7 @@
+// admin.controller.ts
+import { Controller } from "@nestjs/common"
+import { ApiTags } from "@nestjs/swagger"
+
+@ApiTags("Admin")
+@Controller("admin")
+export class AdminController {}
