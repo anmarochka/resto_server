931fa165f514e842b72ced484ca994709bafea5e
Anna Marochkina <annamarochkina2406@gmail.com>
2025-11-28 21:33:12 +0300

    add new

---
+++ src/modules/health/health.controller.ts
@@ -0,0 +1,10 @@
+// health.controller.ts
+import { Controller, Get } from "@nestjs/common"
+
+@Controller("health")
+export class HealthController {
+  @Get()
+  health() {
+    return { status: "ok" }
+  }
+}
